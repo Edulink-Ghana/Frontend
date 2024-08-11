a624c1f05aa90121d99bc1a5053ee8be22bb1d1a
CharlesAduSark <charlesadusarkodie@gmail.com>
2024-08-11 20:22:47 +0000

    Added ext on the section

--- src/landing page/components/about.jsx
+++ src/landing page/components/about.jsx
@@ -1,39 +1,43 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 import '../components/styles.css';
-
-// import required modules
 import { EffectFade, Navigation, Pagination } from 'swiper/modules';
-import { careerImg, heroBg, slider1, slider2, slider3 } from '../../assets';
+import { slider1, slider3 } from '../../assets';
 
 export default function App() {
   return (
-    <>
-      <Swiper
-        spaceBetween={30}
-        effect={'fade'}
-        navigation={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[EffectFade, Navigation, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src={slider1} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slider3} />
-        </SwiperSlide>
-      </Swiper>
-    </>
+    <Swiper
+      spaceBetween={30}
+      effect={'fade'}
+      navigation={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[EffectFade, Navigation, Pagination]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <div className="slider-container">
+          <img src={slider1} className="slider-image" alt="Slide 1" />
+          <div className="overlay">
+            <h2 className="overlay-text">ABOUT US</h2>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="slider-container">
+          <img src={slider3} className="slider-image" alt="Slide 2" />
+          <div className="overlay">
+            <h2 className="overlay-text">Your Text Here for Slide 2</h2>
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 }
